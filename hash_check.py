ddec45bd18b71d7bba12e99a0447079e6a3b766a
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2020-09-08 07:17:50 -0400

    Updated

--- hash_check.py
+++ hash_check.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-
-__author__ = "DFIRSec (@pulsecode)"
-__version__ = "1.0"
-__description__ = "Search through directories for a give file hash (sha256)."
-
 import hashlib
 import os
 import re
@@ -16,19 +10,44 @@ import pandas as pd
 from tabulate import tabulate
 from tqdm import tqdm
 
+__author__ = "DFIRSec (@pulsecode)"
+__version__ = "0.0.2"
+__description__ = "Search through directories for a give file hash (sha256)."
+
 # For Windows systems
 # set TABULATE_INSTALL=lib-only
 
 file_list = []
 
 
-def get_hash(file_path):
+def hash_regex(_str):
+    hash_dict = dict(
+        md5=re.compile(r"\b[A-Fa-f0-9]{32}\b"),
+        sha1=re.compile(r"\b[A-Fa-f0-9]{40}\b"),
+        sha256=re.compile(r"\b[A-Fa-f0-9]{64}\b"),
+        sha512=re.compile(r"\b[A-Fa-f0-9]{128}\b")
+    )
+    match = ''.join([k for k, v in hash_dict.items() if re.match(v, _str)])
+    return match
+
+
+def get_hash(file_path, _str):
+    file_hash = ''
+    hash_str = hash_regex(_str)
     with open(file_path, 'rb') as _file:
-        file_hash = hashlib.sha256(_file.read()).hexdigest()
+        if hash_str == 'md5':
+            file_hash = hashlib.md5(_file.read()).hexdigest()
+        elif hash_str == 'sha1':
+            file_hash = hashlib.sha1(_file.read()).hexdigest()
+        elif hash_str == 'sha256':
+            file_hash = hashlib.sha256(_file.read()).hexdigest()
+        elif hash_str == 'sha512':
+            file_hash = hashlib.sha512(_file.read()).hexdigest()
+
     return file_hash
 
 
-def file_processor(workingdir):
+def file_processor(workingdir, filehash):
     dirpath = Path(workingdir)
     print(f"Scanning: {dirpath} ...")
     for root, _, files in tqdm(os.walk(dirpath),
@@ -42,7 +61,7 @@ def file_processor(workingdir):
                 mtime = time.ctime(os.stat(filepath).st_mtime)
                 fsize = os.stat(filepath).st_size
                 file_list.append((filename, Path(filepath).parent,
-                                  ctime, mtime, fsize, get_hash(filepath)))
+                                  ctime, mtime, fsize, get_hash(filepath, filehash)))
 
             except KeyboardInterrupt:
                 sys.exit('= Exited =')
@@ -55,11 +74,12 @@ def file_processor(workingdir):
 
 
 def main(dirpath, filehash):
-    if not re.match(r"\b[A-Fa-f0-9]{64}\b", filehash):
-        sys.exit("[ERROR] Please enter a valid sha256 hash.")
+    if not hash_regex(filehash):
+        sys.exit(
+            "[ERROR] Please use one of the following: md5, sha1, sha256, sha512")
     else:
         try:
-            file_processor(dirpath)
+            file_processor(dirpath, filehash)
 
             columns = ["File", "Path", "Created", "Modified", "Size (B)", "Hash"]  # nopep8
             df = pd.DataFrame.from_records(file_list, columns=columns)
@@ -81,7 +101,8 @@ if __name__ == "__main__":
      / __  / /_/ (__  ) / / /  / /___/ / / /  __/ /__/ ,<
     /_/ /_/\__,_/____/_/ /_/   \____/_/ /_/\___/\___/_/|_|
 
-    v{__version__}
+                                        v{__version__}
+                                        {__author__}
     """
 
     print(banner)
