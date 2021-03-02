fb3b80b6e84eb866696863285c9c79e8bde321b0
dfirsec <12067255+dfirsec@users.noreply.github.com>
2021-03-02 05:46:03 -0500

    Optimized

--- hash_check.py
+++ hash_check.py
@@ -12,14 +12,14 @@ from tabulate import tabulate
 from tqdm import tqdm
 
 __author__ = "DFIRSec (@pulsecode)"
-__version__ = "v0.0.4"
+__version__ = "v0.0.5"
 __description__ = "Search directories for a give file hash (sha256)."
 
 # terminal colors
 init()
 YEL = Fore.YELLOW
 RST = Fore.RESET
-PROC = f'{YEL}> {RST}'
+PROC = f"{YEL}> {RST}"
 
 # file list holder
 file_list = []
@@ -30,67 +30,78 @@ def hash_regex(_str):
         md5=re.compile(r"\b[A-Fa-f0-9]{32}\b"),
         sha1=re.compile(r"\b[A-Fa-f0-9]{40}\b"),
         sha256=re.compile(r"\b[A-Fa-f0-9]{64}\b"),
-        sha512=re.compile(r"\b[A-Fa-f0-9]{128}\b")
     )
-    match = ''.join([k for k, v in hash_dict.items() if re.match(v, _str)])
+    match = "".join([k for k, v in hash_dict.items() if re.match(v, _str)])
     return match
 
 
-def get_hash(filepath, _str):
-    file_hash = ''
-    hash_str = hash_regex(_str)
-    with open(filepath, 'rb') as _file:
-        if hash_str == 'md5':
-            file_hash = hashlib.md5(_file.read()).hexdigest()
-        elif hash_str == 'sha1':
-            file_hash = hashlib.sha1(_file.read()).hexdigest()
-        elif hash_str == 'sha256':
-            file_hash = hashlib.sha256(_file.read()).hexdigest()
-        elif hash_str == 'sha512':
-            file_hash = hashlib.sha512(_file.read()).hexdigest()
+def get_hash(filename, hash_str, blocksize=65536):
+    global hsh
+    hash_type = hash_regex(hash_str)
 
-    return file_hash
+    if hash_type == "md5":
+        hsh = hashlib.md5()
+    elif hash_type == "sha1":
+        hsh = hashlib.sha1()
+    elif hash_type == "sha256":
+        hsh = hashlib.sha256()
+    elif hash_type == "sha512":
+        hsh = hashlib.sha512()
+
+    with open(filename, "rb") as f:
+        while True:
+            buf = f.read(blocksize)
+            if not buf:
+                break
+            hsh.update(buf)
+    return hsh.hexdigest()
 
 
-def file_processor(workingdir, filehash):
+def walkdir(folder):
+    for root, _, files in os.walk(folder):
+        for filename in files:
+            yield os.path.abspath(os.path.join(root, filename))
+
+
+def file_processor(workingdir, fhash):
     dirpath = Path(workingdir)
     print(f"{PROC}Scanning: {dirpath} ...")
-    for root, _, files in tqdm(os.walk(dirpath),
-                               ascii=True,
-                               desc=f"{PROC}Processing",
-                               ncols=80, unit=" files"):
-        for filename in files:
-            try:
-                filepath = os.path.join(root, filename)
-                ctime = time.ctime(os.stat(filepath).st_ctime)
-                mtime = time.ctime(os.stat(filepath).st_mtime)
-                fsize = os.stat(filepath).st_size
-                file_list.append((filename, Path(filepath).parent,
-                                  ctime, mtime, fsize, get_hash(filepath, filehash)))
+    filecounter = 0
+    for filepath in walkdir(dirpath):
+        filecounter += 1
 
-            except KeyboardInterrupt:
-                sys.exit('= Exited =')
-            except PermissionError:
-                continue
-            except OSError as error:
-                print("OS Error encountered:", error)
-                continue
-    
-def main(dirpath, filehash):
-    if not hash_regex(filehash):
-        sys.exit("\033[31m[ERROR]\033[0m Please use one of the following hash types: MD5, SHA1, SAH256, SHA512")  # nopep8
+    for filepath in tqdm(walkdir(dirpath), total=filecounter, desc=f"{PROC}Processing", ncols=90, unit=" files"):
+        try:
+            ctime = time.ctime(os.stat(filepath).st_ctime)
+            mtime = time.ctime(os.stat(filepath).st_mtime)
+            fsize = os.stat(filepath).st_size
+            file_list.append((filepath, Path(filepath).parent, ctime, mtime, fsize, get_hash(filepath, fhash)))
+
+        except KeyboardInterrupt:
+            sys.exit("= Exited =")
+        except PermissionError:
+            continue
+        except OSError as error:
+            print("OS Error encountered:", error)
+            continue
+
+
+def main(dirpath, fhash):
+    if not hash_regex(fhash):
+        sys.exit("\033[31m[ERROR]\033[0m Please use one of the following hash types: MD5, SHA1, SHA256")
     else:
         try:
-            file_processor(dirpath, filehash)
-            
-            columns = ["File", "Path", "Created", "Modified", "Size (B)", "Hash"]  # nopep8
+            file_processor(dirpath, fhash)
+
+            columns = ["File", "Path", "Created", "Modified", "Size (B)", "Hash"]
             df = pd.DataFrame.from_records(file_list, columns=columns)
 
-            if df.loc[df['Hash'] == filehash].any()[0]:
-                print(f"\n{tabulate(df.loc[df['Hash'] == filehash], showindex=False, headers=columns, tablefmt='github')}")  # nopep8
+            if df.loc[df["Hash"] == fhash].any()[0]:
+                print(
+                    f"\n{tabulate(df.loc[df['Hash'] == fhash], showindex=False, headers=columns, tablefmt='github')}"
+                )
             else:
-                print(f"\n{hash_regex(filehash).upper()} hash '{filehash}' was not found.") #nopep8
-
+                print(f"\n{hash_regex(fhash).upper()} hash '{fhash}' was not found.")
         except Exception as error:
             sys.exit(error)
 
@@ -110,9 +121,9 @@ if __name__ == "__main__":
     print(f"\033[36m{banner}\033[0m")
 
     parser = ArgumentParser()
-    parser.add_argument("dirpath", help="directory path to scan")
-    parser.add_argument("filehash", help="the file hash you're searching for")
+    parser.add_argument("PATH", help="directory path to scan")
+    parser.add_argument("HASH", help="the file hash you're searching for")
     args = parser.parse_args()
-    filehash = args.filehash.lower()
+    HASH = args.HASH.lower()
 
-    main(args.dirpath, filehash)
+    main(args.PATH, HASH)
