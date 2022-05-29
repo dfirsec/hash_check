9ee6b12ef8b9f09378de3d19baf7481081553c67
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2022-05-29 04:48:40 -0400

    Minor Edits

--- hash_check.py
+++ hash_check.py
@@ -29,16 +29,30 @@ file_list = []
 
 
 def hash_regex(hash_str):
+    """
+    Takes a string and returns the name of the hash algorithm that matches the string, or an empty
+    string if no match is found.
+
+    :param hash_str: The string to be checked for a hash
+    :return: The hash type.
+    """
     hash_dict = dict(
         md5=re.compile(r"\b[A-Fa-f0-9]{32}\b"),
         sha1=re.compile(r"\b[A-Fa-f0-9]{40}\b"),
         sha256=re.compile(r"\b[A-Fa-f0-9]{64}\b"),
     )
-    match = "".join([k for k, v in hash_dict.items() if re.match(v, hash_str)])
-    return match
+    return "".join([k for k, v in hash_dict.items() if re.match(v, hash_str)])
 
 
 def gethash(filename, hash_str, blocksize=65536):
+    """
+    Takes a file, a hash algorithm, and a block size, and returns the hash of the file
+
+    :param filename: The file you want to hash
+    :param hash_str: The hash algorithm to use
+    :param blocksize: The size of the chunk of data to read from the file, defaults to 65536 (optional)
+    :return: The hash of the file.
+    """
     algorithm = hash_regex(hash_str)
     hasher = hashlib.new(algorithm)
 
@@ -49,12 +63,25 @@ def gethash(filename, hash_str, blocksize=65536):
 
 
 def walkdir(folder):
+    """
+    Walks through a folder and returns the full path of each file in that folder.
+
+    :param folder: The folder to search for files in
+    """
     for root, _, files in os.walk(folder):
         for filename in files:
             yield Path(root, filename).resolve()
 
 
 def processor(workingdir, fhash):
+    """
+    Takes a directory path and a hash algorithm as arguments, then it walks the directory and creates
+    a dictionary of file metadata for each file in the directory, then it appends the dictionary to a
+    global list.
+
+    :param workingdir: The directory to process
+    :param fhash: the hash algorithm to use
+    """
     dirpath = Path(workingdir)
     print(f"{PROC}Getting file count...", sep=" ", end=" ")
     filecounter = len(list(walkdir(dirpath)))
@@ -89,6 +116,14 @@ def processor(workingdir, fhash):
 
 
 def main(dirpath, fhash, save=None):
+    """
+    Takes a directory path and a hash value as arguments, and then it scans the directory for files,
+    hashes them, and compares the hashes to the hash value provided.
+
+    :param dirpath: The directory to scan
+    :param fhash: The hash you want to search for
+    :param save: If you want to save the results to a file, set this to True
+    """
     if not hash_regex(fhash):
         sys.exit("\033[31m[ERROR]\033[0m Please use one of the following hash types: MD5, SHA1, SHA256")
     else:
@@ -105,14 +140,14 @@ def main(dirpath, fhash, save=None):
         if match.any()[0]:
             print(f"\n{tabulate(match, showindex=False, headers=columns, tablefmt='github')}")
             results = match.to_dict("index")
-            with open("results.json", "w") as _file:
+            with open("results.json", "w", encoding="utf-8") as _file:
                 json.dump(results, _file, indent=4)
             print(f"\n{PROC}Results saved to {CYN}results.json{RST} file")
         else:
             print(f"\n[-] No results for {hash_regex(fhash).upper()} hash: {fhash}")
 
         if file_list and save:
-            with open("hashed_files.json", "w") as _file:
+            with open("hashed_files.json", "w", encoding="utf-8") as _file:
                 json.dump(file_list, _file, indent=4)
 
 
